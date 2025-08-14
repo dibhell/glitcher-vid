4541b3ccd76edc6acd4b4393a983f34ed5b4f299
dibhell <dib.hell@gmail.com>
2025-08-14 12:49:44 +0200

    Update vertex.glsl

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,1 +1,9 @@
-attribute vec2 a_position; attribute vec2 a_texCoord; varying vec2 v_uv; void main(){ v_uv=a_texCoord; gl_Position=vec4(a_position,0.,1.); }
+attribute vec2 a_position;
+attribute vec2 a_texCoord;
+varying vec2 v_uv;
+
+void main() {
+  // Odwracamy współrzędną Y, aby poprawnie wyświetlać wideo i obrazy
+  v_uv = vec2(a_texCoord.x, 1.0 - a_texCoord.y);
+  gl_Position = vec4(a_position, 0.0, 1.0);
+}
