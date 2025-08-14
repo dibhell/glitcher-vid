ea0d119a7caafc2ec4ebcdeb16363661640c0da8
dibhell <dib.hell@gmail.com>
2025-08-14 14:55:36 +0200

    Update 18-wobble.glsl

--- src/shaders/fragment/18-wobble.glsl
+++ src/shaders/fragment/18-wobble.glsl
@@ -1,11 +1,10 @@
 // name: Wobble
 // params: amount
 precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_time; uniform float u_audio; uniform float u_dryWet;
-uniform float u_amount;
+uniform float u_amount; uniform vec2 u_resolution;
 void main() {
     vec3 baseCol = texture2D(u_tex, v_uv).rgb;
     if (u_dryWet < 0.01) { gl_FragColor = vec4(baseCol, 1.0); return; }
-
     float strength = u_amount * 0.05 * u_dryWet;
     vec2 wobbleUV = v_uv;
     wobbleUV.x += sin(v_uv.y * 20.0 + u_time * 2.0) * strength;
