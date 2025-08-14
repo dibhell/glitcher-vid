4883fdefef5af939452a2b7af8b5053e0f2780b5
dibhell <dib.hell@gmail.com>
2025-08-14 11:15:27 +0200

    Create 05-scanlines.glsl

---
+++ src/shaders/fragment/05-scanlines.glsl
@@ -0,0 +1,17 @@
+// name: Core: Scanlines
+// params: amount, glitch
+precision mediump float;
+varying vec2 v_uv;
+uniform sampler2D u_tex;
+uniform float u_audio;
+uniform float u_dryWet;
+uniform float u_amount;
+uniform float u_glitch;
+void main() {
+  vec3 baseCol = texture2D(u_tex, v_uv).rgb;
+  float lineCount = 200.0 + (u_amount * 800.0);
+  float line = mod(v_uv.y * lineCount, 2.0);
+  float darkness = (0.2 + u_glitch * 0.8) * (1.0 + u_audio * 2.0);
+  vec3 finalColor = baseCol * (1.0 - step(1.0, line) * darkness);
+  gl_FragColor = vec4(mix(baseCol, finalColor, u_dryWet), 1.0);
+}
