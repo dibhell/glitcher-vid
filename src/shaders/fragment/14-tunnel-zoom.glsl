3fd9e849962422f2c5eadfd01365bc4d4c6cc53d
dibhell <dib.hell@gmail.com>
2025-08-14 13:44:33 +0200

    Update 14-tunnel-zoom.glsl

--- src/shaders/fragment/14-tunnel-zoom.glsl
+++ src/shaders/fragment/14-tunnel-zoom.glsl
@@ -1,18 +1,15 @@
 // name: Tunnel Zoom
 // params: psy
-precision mediump float;
-varying vec2 v_uv;
-uniform sampler2D u_tex;
-uniform float u_time;
-uniform float u_audio;
-uniform float u_dryWet;
+precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_time; uniform float u_audio; uniform float u_dryWet;
 uniform float u_psy;
 void main() {
     vec3 baseCol = texture2D(u_tex, v_uv).rgb;
+    if (u_dryWet < 0.01) { gl_FragColor = vec4(baseCol, 1.0); return; }
+
     vec2 p = v_uv - 0.5;
     float r = length(p);
     float a = atan(p.y, p.x);
-    float speed = (u_psy + u_audio) * 2.0;
+    float speed = u_psy * 2.0;
     vec2 uv = vec2(1.0 / (r + 0.1), a / 3.14159) + u_time * speed * 0.1;
     vec3 finalColor = texture2D(u_tex, fract(uv)).rgb;
     gl_FragColor = vec4(mix(baseCol, finalColor, u_dryWet), 1.0);
