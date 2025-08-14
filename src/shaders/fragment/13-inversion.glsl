1d29e2a03ade2a96312031dae4ea518972f50e0b
dibhell <dib.hell@gmail.com>
2025-08-14 13:44:17 +0200

    Update 13-inversion.glsl

--- src/shaders/fragment/13-inversion.glsl
+++ src/shaders/fragment/13-inversion.glsl
@@ -1,17 +1,10 @@
 // name: Inversion
-// params: glitch
-precision mediump float;
-varying vec2 v_uv;
-uniform sampler2D u_tex;
-uniform float u_audio;
-uniform float u_dryWet;
-uniform float u_glitch;
+// params: 
+precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_audio; uniform float u_dryWet;
 void main() {
     vec3 baseCol = texture2D(u_tex, v_uv).rgb;
+    if (u_dryWet < 0.01) { gl_FragColor = vec4(baseCol, 1.0); return; }
+
     vec3 finalColor = 1.0 - baseCol;
-    float threshold = 1.0 - clamp(u_glitch + u_audio, 0.0, 1.0);
-    if (u_audio < threshold) {
-        finalColor = baseCol;
-    }
     gl_FragColor = vec4(mix(baseCol, finalColor, u_dryWet), 1.0);
 }
