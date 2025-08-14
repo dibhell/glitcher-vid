f8ba47b1c66ba859d369db3a3f7b31ad03293e93
dibhell <dib.hell@gmail.com>
2025-08-14 13:43:44 +0200

    Update 11-pixelation.glsl

--- src/shaders/fragment/11-pixelation.glsl
+++ src/shaders/fragment/11-pixelation.glsl
@@ -1,14 +1,12 @@
 // name: Pixelation
 // params: amount
-precision mediump float;
-varying vec2 v_uv;
-uniform sampler2D u_tex;
-uniform float u_audio;
-uniform float u_dryWet;
+precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_audio; uniform float u_dryWet;
 uniform float u_amount;
 void main() {
     vec3 baseCol = texture2D(u_tex, v_uv).rgb;
-    float pixelSize = 5.0 + (u_amount + u_audio) * 100.0;
+    if (u_dryWet < 0.01) { gl_FragColor = vec4(baseCol, 1.0); return; }
+
+    float pixelSize = mix(1.0, 150.0, u_amount);
     vec2 pixelatedUV = floor(v_uv * pixelSize) / pixelSize;
     vec3 finalColor = texture2D(u_tex, pixelatedUV).rgb;
     gl_FragColor = vec4(mix(baseCol, finalColor, u_dryWet), 1.0);
