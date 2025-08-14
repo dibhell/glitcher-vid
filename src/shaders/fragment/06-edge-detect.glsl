a60bd36438a4af78791a78f469f32cca6fcf2a53
dibhell <dib.hell@gmail.com>
2025-08-14 11:15:39 +0200

    Create 06-edge-detect.glsl

---
+++ src/shaders/fragment/06-edge-detect.glsl
@@ -0,0 +1,22 @@
+// name: Core: Edge Detection
+// params: amount
+precision mediump float;
+varying vec2 v_uv;
+uniform sampler2D u_tex;
+uniform vec2 u_resolution;
+uniform float u_audio;
+uniform float u_dryWet;
+uniform float u_amount;
+float luminance(vec3 c) { return dot(c, vec3(0.299, 0.587, 0.114)); }
+void main() {
+  vec3 baseCol = texture2D(u_tex, v_uv).rgb;
+  vec2 texel = 1.0 / u_resolution;
+  float threshold = 0.1 + (u_amount * 0.5) * (1.0 - u_audio * 0.8);
+  float N = luminance(texture2D(u_tex, v_uv + vec2(0.0, texel.y)).rgb);
+  float S = luminance(texture2D(u_tex, v_uv - vec2(0.0, texel.y)).rgb);
+  float E = luminance(texture2D(u_tex, v_uv + vec2(texel.x, 0.0)).rgb);
+  float W = luminance(texture2D(u_tex, v_uv - vec2(texel.x, 0.0)).rgb);
+  float sobel = abs(N-S) + abs(E-W);
+  vec3 finalColor = vec3(step(threshold, sobel));
+  gl_FragColor = vec4(mix(baseCol, finalColor, u_dryWet), 1.0);
+}
