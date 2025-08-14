87b8c6c305c1040d3830d20fcf95989bceda30cd
dibhell <dib.hell@gmail.com>
2025-08-14 11:14:13 +0200

    Create 01-classic-glitch.glsl

---
+++ src/shaders/fragment/01-classic-glitch.glsl
@@ -0,0 +1,3 @@
+// name: Core: Classic Glitch
+// params: amount, glitch
+precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_time; uniform float u_audio; uniform float u_dryWet; uniform float u_amount; uniform float u_glitch; void main() { /* ... kod shadera ... */ }
