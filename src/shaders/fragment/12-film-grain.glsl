100a00e722658e7c0ef2b1b133128112549ec9a4
dibhell <dib.hell@gmail.com>
2025-08-14 14:23:41 +0200

    Update 12-film-grain.glsl

--- src/shaders/fragment/12-film-grain.glsl
+++ src/shaders/fragment/12-film-grain.glsl
@@ -1,12 +1,10 @@
 // name: Film Grain
 // params: amount
 precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_time; uniform float u_audio; uniform float u_dryWet;
-uniform float u_amount;
+uniform float u_amount; uniform vec2 u_resolution;
 float hash(vec2 p){ return fract(sin(dot(p, vec2(127.1,311.7))) * 43758.5453); }
 void main() {
     vec3 baseCol = texture2D(u_tex, v_uv).rgb;
-    if (u_dryWet < 0.01) { gl_FragColor = vec4(baseCol, 1.0); return; }
-
     float strength = u_amount * 0.5;
     float grain = (hash(v_uv * u_time) - 0.5) * strength;
     vec3 finalColor = baseCol + grain;
