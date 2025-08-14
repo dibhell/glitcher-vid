0aa80cb8725c437514cdb14a1e5afc0d15db8236
dibhell <dib.hell@gmail.com>
2025-08-14 11:28:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/glitcher-vid/', // <--- DODAJ TĘ LINIĘ
   plugins: [react()],
   assetsInclude: ['**/*.glsl'],
 });
