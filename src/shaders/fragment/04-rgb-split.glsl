d6b60994f163c87242fd19dafa5b3ae5fdc41802
dibhell <dib.hell@gmail.com>
2025-08-14 13:42:37 +0200

    Update 04-rgb-split.glsl

--- src/shaders/fragment/04-rgb-split.glsl
+++ src/shaders/fragment/04-rgb-split.glsl
@@ -1,14 +1,12 @@
 // name: Core: RGB Split
 // params: amount
-precision mediump float;
-varying vec2 v_uv;
-uniform sampler2D u_tex;
-uniform float u_audio;
-uniform float u_dryWet;
+precision mediump float; varying vec2 v_uv; uniform sampler2D u_tex; uniform float u_audio; uniform float u_dryWet;
 uniform float u_amount;
 void main() {
   vec3 baseCol = texture2D(u_tex, v_uv).rgb;
-  float offset = (u_amount * 0.05) + (u_audio * 0.08);
+  if (u_dryWet < 0.01) { gl_FragColor = vec4(baseCol, 1.0); return; }
+
+  float offset = u_amount * 0.05 * u_dryWet;
   float r = texture2D(u_tex, v_uv + vec2(offset, 0.0)).r;
   float g = texture2D(u_tex, v_uv).g;
   float b = texture2D(u_tex, v_uv - vec2(offset, 0.0)).b;
